b6e00b641445763dd63db10945bcac9dc24a5626
HarshKatore <harsh.katore21@pccoepune.org>
2024-12-13 23:28:10 +0530

    Homepage done

---
+++ src/features/restaurants/components/restaurant-info.component.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export const RestaurantInfo = () => {
+  return (
+    <View>
+      <Text>RestaurantInfo</Text>
+    </View>
+  );
+};
+ 
---
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Searchbar } from "react-native-paper";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  StatusBar,
+  Platform,
+} from "react-native";
+import { RestaurantInfo } from "../components/restaurant-info.component";
+
+export const RestaurantsScreen = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.search}>
+        <Searchbar placeholder="Search" />
+      </View>
+
+      <View style={styles.list}>
+        <RestaurantInfo />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  },
+  search: {
+    padding: 16,
+  },
+  list: {
+    flex: 1,
+    padding: 16,
+  },
+});
